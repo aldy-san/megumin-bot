ccdaef0b74a56cc2fee280e566a54c6b8a36abcc
Nur Syahrial Maulidi <>
2021-04-07 04:19:12 +0000

    add RegresiKuadratik

--- main.py
+++ main.py
@@ -16,6 +16,96 @@ dosen = [
   "Yahahawahyu",
   "tes"
 ]
+def getRegKuadratik(input_x, input_y):
+  output = ""
+  if(len(input_x) != len(input_y)):
+    output += "Inputnya salah Blokk!!"
+  else:
+    input_x = np.array([input_x])
+    input_y = np.array([input_y]) 
+    base_tabel = {'xᵢ': [x for x in input_x[0]],
+         'yᵢ': [y for y in input_y[0]],
+         'xᵢ²': [pow(x,2) for x in input_x[0]],
+         'xᵢ³': [pow(x,3) for x in input_x[0]],
+         'xᵢ⁴': [pow(x,4) for x in input_x[0]],
+         'xᵢyᵢ': [x * y for x,y in zip(input_x[0], input_y[0])],
+         'xᵢ²yᵢ': [pow(x,2) * y for x,y in zip(input_x[0], input_y[0])]}
+
+    tabel = pd.DataFrame(base_tabel)
+    base_sum_tabel = dict()
+    for x in tabel:
+        base_sum_tabel[f'Σ{x}'] = tabel[x].sum()
+
+    sum_tabel = pd.DataFrame(base_sum_tabel, index=[0])
+
+    output += tabel.to_string(index=False)
+    output += "\n"
+    output += sum_tabel.to_string(index=False)
+
+    spl_konst = np.array([
+                    [len(input_x[0]), sum_tabel['Σxᵢ'][0], sum_tabel['Σxᵢ²'][0]],
+                    [sum_tabel['Σxᵢ'][0], sum_tabel['Σxᵢ²'][0], sum_tabel['Σxᵢ³'][0]],
+                    [sum_tabel['Σxᵢ²'][0], sum_tabel['Σxᵢ³'][0], sum_tabel['Σxᵢ⁴'][0]]
+                    ])
+    spl_hasil = np.array([
+                          [sum_tabel['Σyᵢ'][0]],
+                          [sum_tabel['Σxᵢyᵢ'][0]],
+                          [sum_tabel['Σxᵢ²yᵢ'][0]]
+                        ])
+
+    output += "\n\nDidapat SPL:\n"
+    output += f"{spl_konst[0][0]}a₀ + {spl_konst[0][1]}a₁ + {spl_konst[0][2]}a₂ = {spl_hasil[0][0]}"
+    output += f"{spl_konst[1][0]}a₀ + {spl_konst[1][1]}a₁ + {spl_konst[1][2]}a₂ = {spl_hasil[1][0]}"
+    output += f"{spl_konst[2][0]}a₀ + {spl_konst[2][1]}a₁ + {spl_konst[2][2]}a₂ = {spl_hasil[2][0]}"
+    spl_ans = np.linalg.solve(spl_konst, spl_hasil)
+    output += "\nSolusi SPL ini adalah:\n"
+    output += f"a₀ = {round(spl_ans[0][0], 5)}"
+    output += f"a₁ = {round(spl_ans[1][0], 5)}"
+    output += f"a₂ = {round(spl_ans[2][0], 5)}"
+    output += "\nRegresi kuadratiknya adalah:\n"
+    output += f"y = {round(spl_ans[0][0], 5)} + {round(spl_ans[1][0], 5)}x + {round(spl_ans[2][0], 5)}x²"
+  return output
+
+def getRegLinear(input_x, input_y):
+  output = ""
+  if(len(input_x) != len(input_y)):
+    output += "Inputnya salah Blokk!!"
+  else: 
+    input_x = np.array([input_x])
+    input_y = np.array([input_y])
+    base_tabel = {'xᵢ': [x for x in input_x[0]],
+        'yᵢ': [y for y in input_y[0]],
+        'xᵢ²': [pow(x,2) for x in input_x[0]],
+        'xᵢyᵢ': [x * y for x,y in zip(input_x[0], input_y[0])]}
+
+    tabel = pd.DataFrame(base_tabel)
+    base_sum_tabel = dict()
+    for x in tabel:
+        base_sum_tabel[f'Σ{x}'] = tabel[x].sum()
+    sum_tabel = pd.DataFrame(base_sum_tabel, index=[0])
+    output += tabel.to_string(index=False)
+    output += "\n"
+    output += sum_tabel.to_string(index=False)
+    output += "\n\nDidapat SPL:\n"
+    output += f"{len(input_x[0])}a₀ + {sum_tabel['Σxᵢ'][0]}a₁ {sum_tabel['Σyᵢ'][0]}"
+
+    spl_konst = np.array([
+      [len(input_x[0]), sum_tabel['Σxᵢ'][0]],
+      [sum_tabel['Σxᵢ'][0], sum_tabel['Σxᵢ²'][0]]
+    ])
+    spl_hasil = np.array([
+      [sum_tabel['Σyᵢ'][0]],
+      [sum_tabel['Σxᵢyᵢ'][0]]
+    ])
+
+    spl_ans = np.linalg.solve(spl_konst, spl_hasil)
+    output += "Solusi SPL ini adalah:\n"
+    output += f"a₀ = {spl_ans[0][0]}"
+    output += f"a₁ = {spl_ans[1][0]}"
+
+    output += "\nRegresi liniarnya adalah:\n"
+    output += f"y = {spl_ans[0][0]} + {spl_ans[1][0]}x"
+  return output
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -29,7 +119,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  output = ""
   if message.author == client.user:
     return
   msg = message.content
@@ -39,9 +128,6 @@ async def on_message(message):
   if message.content.startswith('$megumin'):
     await message.channel.send('Wangy Wangy')
 
-  if message.content.startswith('$quote'):
-    quote = get_quote()
-    await message.channel.send(quote)
 
   if any(word in msg for word in dosen):
     await message.channel.send(random.choice(katain))
@@ -49,49 +135,22 @@ async def on_message(message):
   if msg.startswith('$regLinier'):
     input_msg = msg.split("$regLinier ",1)[1].split(";")
     if(input_msg[0] == "help"):
-      output += "Contoh Input = $regLinier 1 2 3 4 5;1.0 2.0 3.0 4.0 5.0"
+      pesan = "Contoh Input = $regLinier 1 2 3 4 5;1.0 2.0 3.0 4.0 5.0"
     else:
       input_x = [ float(x) for x in input_msg[0].split(" ")]
       input_y = [ float(x) for x in input_msg[1].split(" ")]
-      if(len(input_x) != len(input_y)):
-        output += "Inputnya salah Blokk!!"
-      else: 
-        input_x = np.array([input_x])
-        input_y = np.array([input_y])
-        base_tabel = {'xᵢ': [x for x in input_x[0]],
-            'yᵢ': [y for y in input_y[0]],
-            'xᵢ²': [pow(x,2) for x in input_x[0]],
-            'xᵢyᵢ': [x * y for x,y in zip(input_x[0], input_y[0])]}
+      pesan = getRegLinear(input_x, input_y)
+    await message.channel.send(pesan)
 
-        tabel = pd.DataFrame(base_tabel)
-        base_sum_tabel = dict()
-        for x in tabel:
-            base_sum_tabel[f'Σ{x}'] = tabel[x].sum()
-
-        sum_tabel = pd.DataFrame(base_sum_tabel, index=[0])
-        output += tabel.to_string(index=False)
-        output += "\n"
-        output += sum_tabel.to_string(index=False)
-        output += "\n\nDidapat SPL:\n"
-        output += f"{len(input_x[0])}a₀ + {sum_tabel['Σxᵢ'][0]}a₁ = {sum_tabel['Σyᵢ'][0]}"
-
-        spl_konst = np.array([
-                        [len(input_x[0]), sum_tabel['Σxᵢ'][0]],
-                        [sum_tabel['Σxᵢ'][0], sum_tabel['Σxᵢ²'][0]]
-                        ])
-        spl_hasil = np.array([
-                              [sum_tabel['Σyᵢ'][0]],
-                              [sum_tabel['Σxᵢyᵢ'][0]]
-                            ])
-
-        spl_ans = np.linalg.solve(spl_konst, spl_hasil)
-        output += "Solusi SPL ini adalah:\n"
-        output += f"a₀ = {spl_ans[0][0]}"
-        output += f"a₁ = {spl_ans[1][0]}"
-
-        output += "\nRegresi liniarnya adalah:\n"
-        output += f"y = {spl_ans[0][0]} + {spl_ans[1][0]}x"
-
-    await message.channel.send(output)   
+  if msg.startswith('$regKuadratik'):
+    input_msg = msg.split("$regKuadratik ",1)[1].split(";")
+    await message.channel.send("hai")
+    if(input_msg[0] == "help"):
+      pesan = "Contoh Input = $regKuadratik 1 2 3 4 5;1.0 2.0 3.0 4.0 5.0"
+    else:
+      input_x = [ float(x) for x in input_msg[0].split(" ")]
+      input_y = [ float(x) for x in input_msg[1].split(" ")]
+      pesan = getRegKuadratik(input_x, input_y)
+    await message.channel.send(pesan)
 keep_alive()
 client.run(os.getenv('TOKEN'))
